a989764643f72fa704117294700004679a3f402e
Illei Adrian <illei.adrian@yahoo.com>
2023-09-07 16:38:58 +0200

    Add project files.

---
+++ lab3ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab3ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine(factorial(n));
+        }
+
+        static int factorial(int x)
+        {
+
+            int factorial = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            return factorial;
+
+        }
+    }
+}
